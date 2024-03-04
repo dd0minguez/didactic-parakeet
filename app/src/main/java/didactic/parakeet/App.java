44c0817507d64f464ec01fff22f7aa6fb1d0570f
Denis Domínguez <denisdominguez@Ordenador-portatil-de-Denis.local>
2024-03-04 20:33:40 +0100

    sumar

--- app/src/main/java/didactic/parakeet/App.java
+++ app/src/main/java/didactic/parakeet/App.java
@@ -7,7 +7,9 @@ public class App {
     public String getGreeting() {
         return "Hello World!";
     }
-
+    public int sumar() {
+        return 2 + 2;
+    }
     public static void main(String[] args) {
         System.out.println(new App().getGreeting());
     }
