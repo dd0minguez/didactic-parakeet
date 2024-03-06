cf2dbbf8fd56c3980bcdd22d0498963e0e691b07
Denis Domínguez <denisdominguez@Ordenador-portatil-de-Denis.local>
2024-03-06 15:59:37 +0100

    2

--- app/src/test/java/didactic/parakeet/AppTest.java
+++ app/src/test/java/didactic/parakeet/AppTest.java
@@ -17,6 +17,6 @@ class AppTest {
     }
     @Test void RestaCorrecta(){
         App classResta = new App();
-        assertEquals(2, classResta.sumar(4, 2));
+        assertEquals(3, classResta.sumar(5, 2));
     }
 }
